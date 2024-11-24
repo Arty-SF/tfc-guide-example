6017b22c41219fd1162e6bc79ce8cd23a7f0414d
ArtySF <arty.salesforce@gmail.com>
2024-11-25 03:42:48 +0400

    feat: add sentinel policy as sub-dir - part 10

--- policy-set.tf
+++ policy-set.tf
@@ -1,29 +1,29 @@
 # @see https://registry.terraform.io/providers/hashicorp/tfe/latest/docs/resources/policy_set
 
-#data "tfe_slug" "this" {
-#  source_path = "policies/training-policy-set"
-#}
-#
-#resource "tfe_policy_set" "this" {
-#  name          = "training-policy-set"
-#  description   = "https://hashicorp-terraform.awsworkshop.io/060_sentinel/6-policy-quickstart.html"
-#  organization  = "artysf-org"
-#  workspace_ids = []
-#
-#  // reference the tfe_slug data source.
-#  slug = data.tfe_slug.this
-#}
+data "tfe_slug" "this" {
+  source_path = "policies/training-policy-set"
+}
+
+resource "tfe_policy_set" "this" {
+  name          = "training-policy-set"
+  description   = "https://hashicorp-terraform.awsworkshop.io/060_sentinel/6-policy-quickstart.html"
+  organization  = "artysf-org"
+  workspace_ids = []
+
+  // reference the tfe_slug data source.
+  slug = data.tfe_slug.this
+}
 
 provider "tfe" {
   token        = var.tfe-token
   organization = "artysf-org"
 }
 
-resource "tfe_policy" "test" {
-  name         = "training-policy-set"
-  description  = "https://hashicorp-terraform.awsworkshop.io/060_sentinel/6-policy-quickstart.html"
-  organization = "artysf-org"
-  kind         = "sentinel"
-  policy       = "main = rule { true }"
-  enforce_mode = "hard-mandatory"
-}
+#resource "tfe_policy" "test" {
+#  name         = "training-policy-set"
+#  description  = "https://hashicorp-terraform.awsworkshop.io/060_sentinel/6-policy-quickstart.html"
+#  organization = "artysf-org"
+#  kind         = "sentinel"
+#  policy       = "main = rule { true }"
+#  enforce_mode = "hard-mandatory"
+#}
