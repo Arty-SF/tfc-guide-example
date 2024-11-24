c92e824872b9078ff99c68aeac678f422c7dc397
ArtySF <arty.salesforce@gmail.com>
2024-11-25 03:22:19 +0400

    feat: add sentinel policy as sub-dir - part 5

--- policy-set.tf
+++ policy-set.tf
@@ -2,30 +2,16 @@ data "tfe_slug" "this" {
   source_path = "policies/training-policy-set"
 }
 
-data "tfe_policy_set" "this" {
-  name         = "training-policy-set"
-  organization = "artysf-org"
-}
-
 resource "tfe_workspace" "this" {
   name         = "tfc-guide-example"
   organization = "artysf-org"
 }
 
-import {
-  to = tfe_policy_set.this
-  id = data.tfe_policy_set.this.id
-}
-
 resource "tfe_policy_set" "this" {
-  name                = "training-policy-set"
-  description         = "https://hashicorp-terraform.awsworkshop.io/060_sentinel/6-policy-quickstart.html"
-  organization        = "artysf-org"
-  kind                = "sentinel"
-  agent_enabled       = true
-  policy_tool_version = "latest"
-  overridable         = true
-  workspace_ids       = [tfe_workspace.this.id]
+  name          = "training-policy-set"
+  description   = "https://hashicorp-terraform.awsworkshop.io/060_sentinel/6-policy-quickstart.html"
+  organization  = "artysf-org"
+  workspace_ids = [tfe_workspace.this.id]
 
   // reference the tfe_slug data source.
   slug = data.tfe_slug.this
