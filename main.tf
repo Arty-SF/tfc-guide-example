1ecc3460014719f0086238e912569f923782f82e
ArtySF <arty.salesforce@gmail.com>
2024-11-25 01:33:12 +0400

    fix: move out variables out of tfvars

--- main.tf
+++ main.tf
@@ -13,6 +13,17 @@ terraform {
   }
 }
 
+# @see https://developer.hashicorp.com/terraform/cloud-docs/run/run-environment#environment-variables
+# otherwise @see https://gist.github.com/mattmahn/f46420ba8d740cc008accfde964c2e74
+
+dynamic_tags = {
+    git-commit-sha = var.TFC_CONFIGURATION_VERSION_GIT_COMMIT_SHA
+    git-tag = var.TFC_CONFIGURATION_VERSION_GIT_TAG
+    git-branch = var.TFC_CONFIGURATION_VERSION_GIT_BRANCH
+    run-id = var.TFC_RUN_ID
+}
+
+
 provider "aws" {
   region = var.region
 
--- tags.tf
+++ tags.tf
@@ -1,3 +1,12 @@
 locals {
-  tags = merge(var.static_tags, var.dynamic_tags)
+  tags = merge(
+    {
+      git-commit-sha = var.TFC_CONFIGURATION_VERSION_GIT_COMMIT_SHA
+      git-tag = var.TFC_CONFIGURATION_VERSION_GIT_TAG
+      git-branch = var.TFC_CONFIGURATION_VERSION_GIT_BRANCH
+      run-id = var.TFC_RUN_ID
+    },
+    var.static_tags,
+    var.dynamic_tags
+  )
 }
